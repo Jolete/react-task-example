fa12b48a2050bd9f7fe9ea90d01567b686834b33
Jordi Arenas <jordi.arenas@quantion.com>
2023-04-22 02:16:07 +0200

    first commit

---
+++ src/Components/TaskCard.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskCard({ task }) {
+  // function mostrarAlerta() {
+  //   alert(task.id);
+  // }
+
+  const {deleteTask} =  useContext(TaskContext)
+
+  return (
+    <div className="bg-gray-800 text-white m-1 p-4 rounded-dm">
+      <h1 className="text-xl font-bold capitalize">{task.title}</h1>
+      <p className="text-gray-500 text-sm">{task.description}</p>
+      <button className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
+        onClick={() => {
+          deleteTask(task.id);
+        }}
+      >
+        Eliminar tarea
+      </button>
+    </div>
+  );
+}
+
+export default TaskCard;
